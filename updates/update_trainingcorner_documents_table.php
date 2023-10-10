26a8127e06ffd60284060ef91abc9c972b29622a
root <root@project-dev.pensoft.local>
2023-10-10 13:09:23 +0300

    new field for slideshare

---
+++ updates/update_trainingcorner_documents_table.php
@@ -0,0 +1,22 @@
+<?php namespace Pensoft\TrainingCorner\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class UpdateTrainingCornerDocumentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_trainingcorner_documents', function (Blueprint $table) {
+            $table->text('slideshare')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('pensoft_trainingcorner_documents', function (Blueprint $table) {
+            $table->dropColumn('slideshare');
+        });
+    }
+}
