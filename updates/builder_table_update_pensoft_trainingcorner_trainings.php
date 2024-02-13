30c6c8e1b903f684f47721603395bfa1f7ac5b05
Kremena Kaleva <kkaleva@abv.bg>
2024-02-13 13:29:43 +0200

    default 0

---
+++ updates/builder_table_update_pensoft_trainingcorner_trainings.php
@@ -0,0 +1,23 @@
+<?php namespace Pensoft\TrainingCorner\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdatePensoftTrainingcornerTrainings extends Migration
+{
+    public function up()
+    {
+        Schema::table('pensoft_trainingcorner_trainings', function($table)
+        {
+            $table->integer('sort_order')->default(0)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('pensoft_trainingcorner_trainings', function($table)
+        {
+            $table->integer('sort_order')->default(null)->change();
+        });
+    }
+}
